cf4fe1c0bf18d2028ab0d00c3590f09adcc04339
Daniel Cherubini <daniel@cherubini.casa>
2026-04-03 14:29:31 +0200

    fix: remove standalone kronk-web [[bin]] to eliminate .pdb filename collision on Windows

--- crates/kronk-web/src/main.rs
+++
@@ -1,8 +0,0 @@
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    tracing_subscriber::fmt::init();
-    let addr: std::net::SocketAddr = "0.0.0.0:11435".parse()?;
-    let proxy_base_url =
-        std::env::var("KRONK_PROXY_URL").unwrap_or_else(|_| "http://127.0.0.1:11434".to_string());
-    kronk_web::server::run(addr, proxy_base_url).await
-}
